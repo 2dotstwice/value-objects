dce3b4863f5acd879ec75de609f7adb5fde08e85
Bert Ramakers <bertramakers@me.com>
2018-02-14 16:58:14 +0100

    Make sure the given value is a string when checking if it’s empty or not

--- src/String/Behaviour/IsNotEmpty.php
+++ src/String/Behaviour/IsNotEmpty.php
@@ -9,6 +9,9 @@ trait IsNotEmpty
      */
     private function guardNotEmpty($value)
     {
+        /* @var IsString $this */
+        $this->guardString($value);
+
         if (strlen($value) === 0) {
             throw new \InvalidArgumentException('Given string should not be empty.');
         }
